709c5308a0bc98b5763af6255f47ef8674f45a8f
Mukesh A <amukesh.mk@gmail.com>
2020-07-27 13:20:23 +0100

    added base file with comments for generating training ds

---
+++ generate-training-data.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+
+# write your code here to open your laptop's camera
+# and store images for rock paper scissors
+
+# I would highly recommend storing these images in folders stucture like below:
+# training_data\
+#           |-- empty\
+#           |-- rock\
+#           |-- paper\
+#           |-- scissors\
+# having a folder structure like this will make it easy for you to read the images while pre-processing
